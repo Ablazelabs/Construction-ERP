e9a322c8942485cb444ea5d6df9fdd1c2267fa18
Yared Terefe <yaredterefeg@gmail.com>
2022-02-21 17:18:53 +0300

    finished business overview exports

--- api-routes/finance/businessOverviewExport.js
+++ api-routes/finance/businessOverviewExport.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { error } = require("../../config/config");
-const { profitLossExport } = require("../../services/businessOverviewExport");
+const {
+    profitLossExport,
+    cashFlowExport,
+    balanceSheetExport,
+} = require("../../services/businessOverviewExport");
 
 const inputFilter = require("../../validation/inputFilter");
 
@@ -62,6 +66,130 @@ router.post("/profit_loss", async (req, res, next) => {
     }
     try {
         const data = await profitLossExport(reqBody, res.locals.id, next);
+        if (data == false) {
+            return;
+        }
+        res.json(data);
+    } catch (e) {
+        console.log(e);
+        error("database", "error", next, 500);
+    }
+});
+router.post("/cash_flow", async (req, res, next) => {
+    let reqBody = {};
+    const inputFilterData = {
+        dateRange: "object",
+        reportBasis: "number",
+    };
+    const inputFilterOptionalData = {
+        dateFormat: "string",
+    };
+    try {
+        reqBody = inputFilter(
+            { ...inputFilterData, exportAs: "string" },
+            inputFilterOptionalData,
+            req.body
+        );
+        if (!Array.isArray(reqBody.dateRange)) {
+            throw { key: "dateRange", message: "please send array" };
+        }
+        if (reqBody.dateRange.length != 2) {
+            throw {
+                key: "dateRange",
+                message: "date range must contain two dates(from - to)",
+            };
+        }
+        let enumAndDate = returnReqBody(
+            {
+                reportBasis: reqBody.reportBasis,
+                from: reqBody.dateRange[0],
+                to: reqBody.dateRange[1],
+            },
+            {
+                requiredInputFilter: {
+                    reportBasis: "number",
+                    from: "string",
+                    to: "string",
+                },
+                myEnums: {
+                    reportBasis: enums.general_journal_header.report_basis,
+                },
+                dateValue: ["from", "to"],
+            },
+            next
+        );
+        if (!enumAndDate) return false;
+        reqBody.reportBasis = enumAndDate.reportBasis;
+        reqBody.dateRange[0] = enumAndDate.from;
+        reqBody.dateRange[1] = enumAndDate.to;
+    } catch (e) {
+        error(e.key, e.message, next);
+        return;
+    }
+    try {
+        const data = await cashFlowExport(reqBody, res.locals.id, next);
+        if (data == false) {
+            return;
+        }
+        res.json(data);
+    } catch (e) {
+        console.log(e);
+        error("database", "error", next, 500);
+    }
+});
+router.post("/balance_sheet", async (req, res, next) => {
+    let reqBody = {};
+    const inputFilterData = {
+        dateRange: "object",
+        reportBasis: "number",
+    };
+    const inputFilterOptionalData = {
+        dateFormat: "string",
+    };
+    try {
+        reqBody = inputFilter(
+            { ...inputFilterData, exportAs: "string" },
+            inputFilterOptionalData,
+            req.body
+        );
+        if (!Array.isArray(reqBody.dateRange)) {
+            throw { key: "dateRange", message: "please send array" };
+        }
+        if (reqBody.dateRange.length != 2) {
+            throw {
+                key: "dateRange",
+                message: "date range must contain two dates(from - to)",
+            };
+        }
+        let enumAndDate = returnReqBody(
+            {
+                reportBasis: reqBody.reportBasis,
+                from: reqBody.dateRange[0],
+                to: reqBody.dateRange[1],
+            },
+            {
+                requiredInputFilter: {
+                    reportBasis: "number",
+                    from: "string",
+                    to: "string",
+                },
+                myEnums: {
+                    reportBasis: enums.general_journal_header.report_basis,
+                },
+                dateValue: ["from", "to"],
+            },
+            next
+        );
+        if (!enumAndDate) return false;
+        reqBody.reportBasis = enumAndDate.reportBasis;
+        reqBody.dateRange[0] = enumAndDate.from;
+        reqBody.dateRange[1] = enumAndDate.to;
+    } catch (e) {
+        error(e.key, e.message, next);
+        return;
+    }
+    try {
+        const data = await balanceSheetExport(reqBody, res.locals.id, next);
         if (data == false) {
             return;
         }
